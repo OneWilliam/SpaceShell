bda6822f5151060753ec15e0551c3195f92c3eaa
OneWilliam <wyvargasc@unjbg.edu.pe>
2025-10-13 10:23:55 -0500

    docs(prompt): agregar documentación inicial con Doxide

--- include/prompt.hpp
+++ include/prompt.hpp
@@ -1,7 +1,19 @@
 #pragma once
 #include <string>
 
+/**
+ * Responsable de generar y mostrar el indicador de la shell.
+ *
+ * Encapsula la lógica para crear un prompt que puede mostrar
+ * información como el directorio de actual.
+ */
 class Prompt {
  public:
+  /**
+   * Muestra el prompt formateado en la salida estándar.
+   *
+   * @tip Se planear hacer uso de algunas funciones builtins para mejorar
+   * la funciones del prompt.
+   */
   void mostrar();
 };
