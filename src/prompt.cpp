a4f7b87286bca671a1c02959ef62b0633f88eb2c
OneWilliam <wyvargasc@unjbg.edu.pe>
2025-10-12 22:30:28 -0500

    style: formatear codigo con clang-format

--- src/prompt.cpp
+++ src/prompt.cpp
@@ -1,17 +1,18 @@
 #include "prompt.hpp"
-#include <iostream>
+
 #include <unistd.h>
+
 #include <climits>
+#include <iostream>
 
 using namespace std;
 
-
 void Prompt::mostrar() {
-    char cwd_buffer[PATH_MAX];
-    if (getcwd(cwd_buffer, sizeof(cwd_buffer)) != nullptr) {
-        cout << cwd_buffer << "@ " << flush;
-    } else {
-        perror("[SHELL] Error al obtener el directorio actual para el prompt");
-        cout << "[?] >>> " << flush;
-    }
+  char cwd_buffer[PATH_MAX];
+  if (getcwd(cwd_buffer, sizeof(cwd_buffer)) != nullptr) {
+    cout << cwd_buffer << "@ " << flush;
+  } else {
+    perror("[SHELL] Error al obtener el directorio actual para el prompt");
+    cout << "[?] >>> " << flush;
+  }
 }
