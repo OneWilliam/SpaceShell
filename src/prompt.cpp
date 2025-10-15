beeb3de147d1ac7fecffe900eb94b93103f6975d
OneWilliam <wyvargasc@unjbg.edu.pe>
2025-10-15 08:19:25 -0500

    feat: colores básicos para el prompt

--- src/prompt.cpp
+++ src/prompt.cpp
@@ -10,7 +10,7 @@ using namespace std;
 void Prompt::mostrar() {
   char cwd_buffer[PATH_MAX];
   if (getcwd(cwd_buffer, sizeof(cwd_buffer)) != nullptr) {
-    cout << cwd_buffer << "@ " << flush;
+    cout <<  "\033[36m" << cwd_buffer << "\033[33m" << "@ " << "\033[0m"  << flush;
   } else {
     perror("[SHELL] Error al obtener el directorio actual para el prompt");
     cout << "[?] >>> " << flush;
